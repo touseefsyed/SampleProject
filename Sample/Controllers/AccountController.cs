d11949d2c4093b773613d445a64828a19b719324
touseefsyed <touseefsy@gmail.com>
2020-05-29 02:03:22 +0500

    first commit

---
+++ Sample/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User obj)
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(User obj)
+        {
+            return View();
+        }
+    }
+}
---
+++ Sample/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Sample.Models
+{
+    [Table("User", Schema = "Users")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfrimPassword { get; set; }
+    }
+}
